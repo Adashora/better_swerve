580dad8b9db612e3e7cc36233bfc0ce8cec6801e
Abhinav dashora <abhinavdashora@Abhinavs-Air.hsd1.mn.comcast.net>
2025-02-06 17:09:30 -0600

    added comments and formatted

--- src/main/java/frc/robot/subsystems/Swerve_module.java
+++ src/main/java/frc/robot/subsystems/Swerve_module.java
@@ -21,133 +21,122 @@ import frc.robot.Constants;
 
 /** Add your docs here. */
 public class Swerve_module {
-public  SparkMax drive_motor;
-public  SparkMax turn_motor;
-public  double module_number;
+    public SparkMax drive_motor;
+    public SparkMax turn_motor; // defining variables
+    public double module_number;
 
-public double turn_speed;
-public double drive_speed;
-PIDController turn_PID;
-PIDController drive_PID;
+    public double turn_speed;
+    public double drive_speed;
+    PIDController turn_PID;
+    PIDController drive_PID;
 
-Rotation2d turn_offset;
+    Rotation2d turn_offset;
 
-private final CANCoder best_turnEncoder;
-private AbsoluteSensorRange range;
-private final RelativeEncoder drive_encoder;
-private final RelativeEncoder turn_encoder;
+    private final CANCoder best_turnEncoder;
+    private AbsoluteSensorRange range;
+    private final RelativeEncoder drive_encoder;
+    private final RelativeEncoder turn_encoder;
 
-private final SparkMaxConfig drive_config;
-private final SparkMaxConfig turn_config;
+    private final SparkMaxConfig drive_config;
+    private final SparkMaxConfig turn_config;
 
-    public Swerve_module(int module_number, int drive_motor_ID, int turn_motor_ID, int cancoder_ID, Rotation2d turn_offset) {
+    public Swerve_module(int module_number, int drive_motor_ID, int turn_motor_ID, int cancoder_ID,
+            Rotation2d turn_offset) {
         this.turn_offset = turn_offset;
         this.drive_config = new SparkMaxConfig();
         this.turn_config = new SparkMaxConfig();
-    
-       this.module_number = module_number;
-      
 
-        
-      
-       
-       
-       
-       //cancoder stuff
-       this.best_turnEncoder = new CANCoder(cancoder_ID); 
-       range = AbsoluteSensorRange.Signed_PlusMinus180; 
-        this.best_turnEncoder.configAbsoluteSensorRange(range); //sets the range of the encoder
-     
-     
-       //drive motors stuff
-        this.drive_motor = new SparkMax(drive_motor_ID, MotorType.kBrushless); //defines drive motor
-        this.drive_encoder = this.drive_motor.getEncoder(); //gerts the encoder fomr the dirve motor
-    
+        this.module_number = module_number;
+
+        // cancoder stuff
+        this.best_turnEncoder = new CANCoder(cancoder_ID);
+        range = AbsoluteSensorRange.Signed_PlusMinus180;
+        this.best_turnEncoder.configAbsoluteSensorRange(range); // sets the range of the encoder
+
+        // drive motors stuff
+        this.drive_motor = new SparkMax(drive_motor_ID, MotorType.kBrushless); // defines drive motor
+        this.drive_encoder = this.drive_motor.getEncoder(); // gerts the encoder fomr the dirve motor
+
         drive_config
-            .voltageCompensation(12)
-            .smartCurrentLimit(50)
-            .idleMode(IdleMode.kBrake)
-            .inverted(false);
-        drive_config.encoder
-            .positionConversionFactor(Constants.dt.position_conversion_factor)
-            .velocityConversionFactor(Constants.dt.velocity_conversion_factor);
-         //sets the current limit to 40 amps
-        
+                .voltageCompensation(12) // configs for drive
+                .smartCurrentLimit(50)
+                .idleMode(IdleMode.kBrake)
+                .inverted(false);
+        drive_config.encoder // configs for encoder
+                .positionConversionFactor(Constants.dt.position_conversion_factor)
+                .velocityConversionFactor(Constants.dt.velocity_conversion_factor);
+        // sets the current limit to 40 amps
 
         this.drive_motor.configure(turn_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-       
-        drive_PID = new PIDController(Constants.dt.drivekp, Constants.dt.driveki, Constants.dt.drivekd);
 
+        drive_PID = new PIDController(Constants.dt.drivekp, Constants.dt.driveki, Constants.dt.drivekd); //
 
-        //turn motor stuff
-        turn_motor = new SparkMax(turn_motor_ID, MotorType.kBrushless); //defines turn motor
-        this.turn_encoder = this.turn_motor.getEncoder(); //gets the encoder from the turn motor
-        
-        
+        // turn motor stuff
+        turn_motor = new SparkMax(turn_motor_ID, MotorType.kBrushless); // defines turn motor
+        this.turn_encoder = this.turn_motor.getEncoder(); // gets the encoder from the turn motor
+
         best_turnEncoder.configAbsoluteSensorRange(range);
 
         turn_config
-            .voltageCompensation(12)
-            .smartCurrentLimit(20)
-            .idleMode(IdleMode.kBrake)
-            .inverted(false);
+                .voltageCompensation(12)
+                .smartCurrentLimit(20) // limits the current to 20 amps
+                .idleMode(IdleMode.kBrake)
+                .inverted(false);
         turn_config.encoder
-            .positionConversionFactor(360)
-            .velocityConversionFactor(360 / 60);
+                .positionConversionFactor(360) // converts the position to degrees
+                .velocityConversionFactor(360 / 60); // converts the velocity to degrees per second
         turn_config.signals
-            .primaryEncoderPositionPeriodMs(500);
-          
-            this.turn_motor.configure(turn_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+                .primaryEncoderPositionPeriodMs(500); // sets the update period of the encoder to 500 ms
 
-        
-            
+        this.turn_motor.configure(turn_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-       turn_PID = new PIDController(Constants.dt.turnKP, Constants.dt.turnKI, Constants.dt.turnKD);
-      
-
+        turn_PID = new PIDController(Constants.dt.turnKP, Constants.dt.turnKI, Constants.dt.turnKD);
 
     }
 
-    public Rotation2d getCANCoder() {
+    public Rotation2d getCANCoder() { // gets the angle of the cancoder
         double angle = this.best_turnEncoder.getAbsolutePosition();
         return Rotation2d.fromDegrees(angle);
     }
 
-    public SwerveModuleState getState() {
+    public SwerveModuleState getState() { // gets the state of the module
         double speedMetersPerSecond = this.drive_encoder.getVelocity(); // * Constants.dt.velocity_conversion_factor;
         Rotation2d angle = getCANCoder();
         return new SwerveModuleState(speedMetersPerSecond, angle);
     }
 
-    public void setDesiredState(SwerveModuleState desiredState) {
+    public void setDesiredState(SwerveModuleState desiredState) { // sets the desired state of the module
         SwerveModuleState current_state = this.getState();
 
+        Rotation2d current_rotation = current_state.angle.minus(this.turn_offset).plus(new Rotation2d()); /* gets the
+                                                                                                             current
+                                                                                                             rotation of
+                                                                                                             the module */
 
-        Rotation2d current_rotation = current_state.angle.minus(this.turn_offset).plus(new Rotation2d());
+        desiredState.optimize(current_rotation);
 
-       desiredState.optimize(current_rotation);
+        desiredState.cosineScale(current_rotation); /* scales the desired state by the cosine of the difference between
+                                                    the desired state and the current state
+                                                    if angle dif is small then motor power is faster
+                                                    if angle dif is big then motor power is slower I think */
 
-       desiredState.cosineScale(current_rotation);
+        Rotation2d desired_rotation = desiredState.angle.plus(new Rotation2d());
 
-       Rotation2d desired_rotation = desiredState.angle.plus(new Rotation2d());
+        double diff = desired_rotation.getDegrees() - current_rotation.getDegrees(); /*gets the difference between the
+                                                                                       desired rotation and the current
+                                                                                       rotation */
 
-       double diff = desired_rotation.getDegrees() - current_rotation.getDegrees();
+        if (Math.abs(diff) < 1) { // if the difference is less than 1 then the turn speed is 0
+            turn_speed = 0;
+        } else { // otherwise the turn speed is calculated by the PID controller
+            turn_speed = turn_PID.calculate(diff, 0);
+        }
 
-       if (Math.abs(diff) < 1){
-              turn_speed = 0;
-       }
-         else{
-              turn_speed = turn_PID.calculate(diff, 0);
-         }
+        drive_speed = desiredState.speedMetersPerSecond / Constants.dt.max_speed;
 
+        turn_motor.set(turn_speed); // sets the turn speed
+        drive_motor.set(drive_speed); // sets the drive speed
 
-         drive_speed = desiredState.speedMetersPerSecond/Constants.dt.max_speed;
-        
-        
-         turn_motor.set(turn_speed);
-        drive_motor.set(drive_speed);
-        
     }
 
 }
-
