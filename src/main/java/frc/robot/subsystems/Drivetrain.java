3f33c8e9d0b42a52352b10b66651ae479db19071
Abhinav dashora <abhinavdashora@Abhinavs-Air.hsd1.mn.comcast.net>
2025-02-06 22:44:10 -0600

    added comments and format

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -15,63 +15,62 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
- 
 
   public PigeonIMU gyro;
 
-public Swerve_module [] hehe;
-  
+  public Swerve_module[] hehe;
 
-  
   /** Creates a new Drivetrain. */
   public Drivetrain() {
 
     set_gyro(0);
 
-this.hehe = new Swerve_module []{
+    this.hehe = new Swerve_module[] { // sets up the swerve module instances in an array
 
-  new Swerve_module(0, Constants.dt.Module_0.drive_id, Constants.dt.Module_0.turn_id, 0, Constants.dt.Module_0.turn_offset),
-  new Swerve_module(1, Constants.dt.Module_1.drive_id, Constants.dt.Module_1.turn_id, 0, Constants.dt.Module_1.turn_offset),
-  new Swerve_module(2,  Constants.dt.Module_2.drive_id, Constants.dt.Module_2.turn_id, 0, Constants.dt.Module_2.turn_offset),
-  new Swerve_module(3,  Constants.dt.Module_3.drive_id, Constants.dt.Module_3.turn_id, 0, Constants.dt.Module_3.turn_offset)
-};
+        new Swerve_module(0, Constants.dt.Module_0.drive_id, Constants.dt.Module_0.turn_id, 0,
+            Constants.dt.Module_0.turn_offset),
+        new Swerve_module(1, Constants.dt.Module_1.drive_id, Constants.dt.Module_1.turn_id, 0,
+            Constants.dt.Module_1.turn_offset),
+        new Swerve_module(2, Constants.dt.Module_2.drive_id, Constants.dt.Module_2.turn_id, 0,
+            Constants.dt.Module_2.turn_offset),
+        new Swerve_module(3, Constants.dt.Module_3.drive_id, Constants.dt.Module_3.turn_id, 0,
+            Constants.dt.Module_3.turn_offset)
+    };
 
-   gyro = new PigeonIMU(10);
+    gyro = new PigeonIMU(10); // defines gyro
 
-   gyro.configFactoryDefault();
+    gyro.configFactoryDefault(); // gyro config
 
   }
 
-public void set_gyro (double yaw){
-  gyro.setYaw(yaw);
-}
-
-public Rotation2d get_yaw() {
-
- return Rotation2d.fromDegrees(gyro.getYaw());
-
-}
-
-public void drive (Translation2d translation, double rotation, boolean isfieldrelative){
-
-  SwerveModuleState[] states = Constants.Swerve_map.toSwerveModuleStates(
-    isfieldrelative ? ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotation, get_yaw())
-                    : new ChassisSpeeds(translation.getX (), translation.getY(), rotation)
-  );
-
-  SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.max_speed);
-  
-  for (Swerve_module module : this.hehe) {
-    module.set_desired_state(states[module.module_number]);
+  public void set_gyro(double yaw) { // sets gyro
+    gyro.setYaw(yaw);
   }
 
-}
+  public Rotation2d get_yaw() { // gets yaw
 
+    return Rotation2d.fromDegrees(gyro.getYaw());
 
+  }
+
+  public void drive(Translation2d translation, double rotation, boolean isfieldrelative) {
+
+    SwerveModuleState[] states = Constants.Swerve_map.toSwerveModuleStates( /* if field relatie is true converts sped
+                                                                            using yaw angle */
+        isfieldrelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotation, get_yaw())
+            : new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
+
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.max_speed); // no wheel goes faster than max
+
+    for (Swerve_module module : this.hehe) {
+      module.set_desired_state(states[module.module_number]);
+    }
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
-
