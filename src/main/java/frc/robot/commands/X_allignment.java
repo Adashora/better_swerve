5ca7a5b2b34da69e1c88593ab41224d490576717
Abhinav Dashora <dashoraabhinav@gmail.com>
2025-03-25 17:32:58 -0500

    uncommented vision code that def doesnt work

--- src/main/java/frc/robot/commands/X_allignment.java
+++ src/main/java/frc/robot/commands/X_allignment.java
@@ -1,61 +1,61 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.filter.SlewRateLimiter;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.Constants;
-// import frc.robot.subsystems.Drivetrain;
-// import frc.robot.subsystems.Vision;
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class X_allignment extends Command {
 
-// /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-// public class X_allignment extends Command {
+Vision vision;
+Drivetrain drivetrain;
+SlewRateLimiter x_limit = new SlewRateLimiter(3);
 
-//   Vision vision;
-//   Drivetrain drivetrain;
-//   SlewRateLimiter x_limit = new SlewRateLimiter(3);
-
-//   double x_offset;
-//   double x;
-//   PIDController PID_vision;
-
+double x_offset;
+double x;
+PIDController PID_vision;
 
 
-//   /** Creates a new X_allignment. */
-//   public X_allignment(Vision vision, Drivetrain drivetrain) {
 
-//     this.x_offset = vision.get_x_offset();
-//     this.vision = vision;
-//     this.drivetrain = drivetrain;
+// Creates a new X_allignment. 
+ public X_allignment(Vision vision, Drivetrain drivetrain) {
 
-//     PID_vision = new PIDController(Constants.vision.vision_p, Constants.vision.vision_i, Constants.vision.vision_d);
-//     // Use addRequirements() here to declare subsystem dependencies.
-//   }
-//   // Called when the command is initially scheduled.
-//   @Override
-//   public void initialize() {}
+this.x_offset = vision.get_x_offset();
+this.vision = vision;
+this.drivetrain = drivetrain;
 
-//   // Called every time the scheduler runs while the command is scheduled.
-//   @Override
-//   public void execute() {
+PID_vision = new PIDController(Constants.vision.vision_p, Constants.vision.vision_i, Constants.vision.vision_d);
+// Use addRequirements() here to declare subsystem dependencies.
+}
+//   Called when the command is initially scheduled.
+@Override
+public void initialize() {}
 
-//     if (this.vision.See_tag()){
-//       x = x_limit.calculate(PID_vision.calculate(x_offset, 0));
+//   Called every time the scheduler runs while the command is scheduled.
+   @Override
+   public void execute() {
 
-//     }
+    if (this.vision.See_tag()){
+      x = x_limit.calculate(PID_vision.calculate(x_offset, 0));
 
-//   }
+     }
 
-//   // Called once the command ends or is interrupted.
-//   @Override
-//   public void end(boolean interrupted) {}
+  }
 
-//   // Returns true when the command should end.
-//   @Override
-//   public boolean isFinished() {
-//     return false;
-//   }
-// }
+   // Called once the command ends or is interrupted.
+   @Override
+   public void end(boolean interrupted) {}
+
+   // Returns true when the command should end.
+      @Override
+   public boolean isFinished() {
+     return false;
+   }
+}
+ 
